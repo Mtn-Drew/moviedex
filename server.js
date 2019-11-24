6905c43ae68d804659ac855c420d59360e42c5a9
Drew Carrillo <drewc3@gmail.com>
2019-11-24 09:04:35 -0700

    Working

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const POKEDEX = require('./pokedex.json')
+const MOVIES = require('./movies-data-small.json')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -18,34 +22,33 @@ app.use(function validateBearerToken(req, res, next) {
   next()
 })
 
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = MOVIES.movie;
 
-app.get('/types', function handleGetTypes(req, res) {
-  res.json(validTypes)
-})
-
-app.get('/pokemon', function handleGetPokemon(req, res) {
-  let response = POKEDEX.pokemon;
-
-  // filter our pokemon by name if name query param is present
-  if (req.query.name) {
-    response = response.filter(pokemon =>
+  if (req.query.genre) {
+    response = response.filter(movie =>
       // case insensitive searching
-      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     )
   }
 
-  // filter our pokemon by type if type query param is present
-  if (req.query.type) {
-    response = response.filter(pokemon =>
-      pokemon.type.includes(req.query.type)
+  if (req.query.country) {
+    response = response.filter(movie =>
+      // case insensitive searching
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
     )
   }
 
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      // case insensitive searching
+      movie.avg_vote >=req.query.avg_vote
+    )
+  }
   res.json(response)
 })
 
-const PORT = 8000
+const PORT = 8008
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
